268e5e364b480062797e4590de4b758cc98b2b47
ricaportela <ricaportela@gmail.com>
2021-07-11 09:54:34 -0300

    Fix url path todos

--- backend/todo_api/urls.py
+++ backend/todo_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("todos.urls")),
+    path("api/v1/todos/", include("todos.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
